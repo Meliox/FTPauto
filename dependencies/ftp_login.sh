365deca44cd17854cb4de2a4355459d48c8c3fa2
Steffe <you@example.com>
2013-08-21 15:18:51 +0200

    fixes version search

---
+++ dependencies/ftp_login.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+function ftp_login {
+	if ((verbose)); then
+		echo "debug 8 -t -o $lftpdebug" >> "$ftplogin_file"
+	fi
+		#Timeout settings
+		echo "set net:timeout 10" >> "$ftplogin_file"
+	#	echo "set net:max-retries 5" >> "$ftplogin_file"
+	#	echo "set net:reconnect-interval-base 5" >> "$ftplogin_file"
+	#	echo "set net:reconnect-interval-multiplier 2" >> "$ftplogin_file"
+	if [[ $ssl == "true" ]]; then
+		echo "set ftp:ssl-force true" >> "$ftplogin_file"
+		echo "set ssl:verify-certificate false" >> "$ftplogin_file"
+	fi
+	if  [[ $transferetype =~ "upftp" ]] || [[ $transferetype =~ "downftp" ]] || [[ $transferetype =~ "fxp" ]]; then
+		echo "open -u $ftpuser,$ftppass $ftphost -p $ftpport" >> "$ftplogin_file"
+	else
+		echo -e "\e[00;31mERROR: Logintype not supported\e[00m"
+	fi
+}
