6e18c24b2f4f7172a95703e13d77ea1650a2d1bf
Steffe <silent.ftp@gmail.com>
2014-02-23 13:15:35 +0100

    Ftp_list changes: - Fix: ftplist file getting created in root dir - Fix: listing of files - New: Jump to rootpath on ftpserver - Changed: Download is now started by adding d after number - Changed: The logic behind choice now works better

--- dependencies/ftp_list.sh
+++ dependencies/ftp_list.sh
@@ -2,36 +2,34 @@
 function ftp_list {
 	echo "INFO: Looking up content on ftp..."
 	if [[ -z $dir ]]; then
-		dir=""
+		dir="/"
 	fi
 	while true; do
 		get_content "$dir"
-		read -p " Enter number to expand directory or download? (x to exit) "
+		read -p "Enter number to expand directory or download by also adding a \"d\", fx. 1d ? (x to exit) "
 		# make sure number is inserted
 		while ! [[ "$REPLY" =~ [0-9] ]] && [[ $REPLY != "x" ]]; do
-			read -p " Enter number to expand directory or download? (x to exit) "
-		done
-		if [[ "$REPLY" == "x" ]]; then
-			break
-		fi			
+			read -p " Enter number to expand directory or download by also adding a \"d\", fx. 1d ? (x to exit) "
+		done			
 		number=$REPLY
-		if [[ $number -eq 0 ]]; then
-			#remove last path component
+		if [[ "$number" == "x" ]]; then
+			break
+		elif [[ "$number" == "0" ]]; then
+			# go to top path
+			dir="/"
+			continue
+		elif [[ "$number" == "1" ]]; then
+			# remove last path component
 			dir=$(dirname "$dir")
 			continue
-		else
-			dir="$dir$(echo ${array_list[$number]} | awk '{print $9}')"
-		fi
-		read -p " Do you want to List or Download (0/1)? "
-		if [[ $REPLY -eq 0 ]]; then
-			# list new dir
-			continue
-		else
-			# make download argument
+		elif [[ "$number" =~ ^[0-9]+d ]]; then
+			# extract number
+			number=${number%d}
+			# make download
+			path="$dir$(echo ${array_list[$number]} | awk '{print $9}')"
 			download_argument+=("--user=$username")
-			download_argument+=("--path=/$dir")
+			download_argument+=("--path=/$path")
 			download_argument+=("--source=Manually")
-			path="/$dir"
 			# change option
 			option[0]="download"
 			read -p " Do you want download it now(y/n)? "
@@ -46,21 +44,30 @@ function ftp_list {
 				fi
 			else
 				# todo. Bad fix to avoid starting download
+				echo " Adding $path to queue"
 				continue_queue="false"
 				option[1]="queue"
 				main &> /dev/null &
 			fi
 			# unset the added path and stay in current directory.
 			unset path download_argument
-			dir=$(dirname "$dir")
+		else
+			# do normal listing
+			if [[ "$dir$(echo ${array_list[$number]} | awk '{print $9}')" != */ ]]; then
+				# not a directory
+				echo -e "\e[00;31m Cannot expand file: $(echo ${array_list[$number]} | awk '{print $9}')\e[00m"
+				continue
+			fi
+			dir="$dir$(echo ${array_list[$number]} | awk '{print $9}')"
+			continue
 		fi
 	done
+	
+	# Ask to start download if something has been added
 	if [[ -e "$queue_file" ]]; then
-		read -p " Do you want to start the download (y/n)?"
+		read -p " Do you want to start the download (y/n)? "
 		if [[ $REPLY == "y" ]]; then
 			background=true
-			main &> /dev/null &
-		else
 			main
 		fi
 	fi
@@ -71,16 +78,13 @@ function get_content {
 	source "$scriptdir/dependencies/ftp_login.sh" && ftp_login # we need to generate a new each time as download removes it
 	cat "$ftplogin_file" >> "$ftplist_file"
 	echo "ls -aFl "$dir" > ~/../..$ftp_content" >> "$ftplist_file"
-	echo "quit" >> "$$ftplist_file"
+	echo "quit" >> "$ftplist_file"
 	$lftp -f "$ftplist_file" &> /dev/null
 	if [[ $? -eq 0 ]]; then
-		echo "INFO: Listing content(s):"
+		echo ""
+		echo -e "\e[00;32mINFO: Listing content(s):\e[00m"
 		echo " Current path: $dir"
 		old_dir="$dir"
-		# remove /./ as it is not needed
-		if [[ -f "$ftp_content" ]]; then
-			sed "1d" -i "$ftp_content"
-		fi
 		array_list=()
 		readarray array_list < "$ftp_content"
 		i=0
