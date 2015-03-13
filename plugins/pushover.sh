d7f8a46a9748fa56a9b9d1d4109776d4eb06c56c
Steffe <silent.ftp@gmail.com>
2015-03-13 13:18:45 +0100

    Revert "Fix for naming of directory when transering files"
    This reverts commit d36b43bb2c524628b06e7d18c2cfc52ccd466033.

--- plugins/pushover.sh
+++ plugins/pushover.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #Shell-script wrapper around curl for sending messages through PushOver
 #For more info, see https://pushover.net/
-version="1.0"
+version="1.1"
 rev=1
 message="$1"
 #send message through curl
 CURL="$(which curl)"
 PUSHOVER_URL="https://api.pushover.net/1/messages"
-if [[ -n "$push_token" ]] || [[ -n "$push_user" ]] || [[ -n "$message" ]]; then
+if [[ -n "$push_token" ]] && [[ -n "$push_user" ]] && [[ -n "$message" ]]; then
 	echo "INFO: Sending pushnotification"
 	curl_cmd="\"${CURL}\" -s \
 	   -F \"token=${push_token}\" \
@@ -17,5 +17,5 @@ if [[ -n "$push_token" ]] || [[ -n "$push_user" ]] || [[ -n "$message" ]]; then
 	eval "${curl_cmd}"
 	echo "INFO: Pushnotification sent"
 else
-	echo -e "\e[00;31mERROR: Pushnotifications not set\e[00m"
+	echo -e "\e[00;31mERROR: All settings are not set.\e[00m"
 fi
