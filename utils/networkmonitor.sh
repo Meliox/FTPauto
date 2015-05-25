79e89f69e1b3506fd2dfc2ec40e8a9dc8077c6d3
Steffe <silent.ftp@gmail.com>
2015-05-25 19:48:48 +0200

    Update networkmonitor to v0.8 - Improve reading of data from iptables by making specific chains - Added debug functionality - Fixed if else bugs - Added test argument to follow traffic each 5th second for testing purposes - Improve documentation

--- utils/networkmonitor.sh
+++ utils/networkmonitor.sh
@@ -1,97 +1,120 @@
 #!/bin/sh
-version="0.7"
+version="0.8"
 # Network traffic monitor
-# Purpose is to shut down server or do anything else if network traffic is below threshold due to
-# low activity
+# Purpose is to shut down computer or run a custom command if network traffic is below threshold due to low activity or a certain ip is not online
 
+# The script can either monitor traffic from the netcard or use iptables. See below:
+####
+# Netcard:
 # Do you want to monitor your netcard or the firewall? The netcard cannot see more traffic than
 # allow by the bit of your system, i.e. 2^32 = 4gb. (32bit limit, 64bit goes to 2.3exabytes). A wrap has been used to
 # fix this while using netcard, but total traffic cannot be seen for fast speeds. At low speeds the purpose of the
-# script works. For correct transfer, the 64bit system is required or the use of iptables.
+# script works. For correct transfer, a 64bit system is required, or one may simply use of iptables.
 #
-# Proper traffic count can be seen with iptables, but
-# this requires to set op iptables rules like below
+####
+# Iptables
+# Proper traffic count can be seen with iptables, but this requires to set op iptables rules like below
 # Add monitor for INPUT and OUTPUT. REQUIRED!
-# iptables -A INPUT -j ACCEPT
-# iptables -A OUTPUT -j ACCEPT
+# root@:~# iptables -N trafficMonitor_in
+# root@:~# iptables -N trafficMonitor_out
+# root@:~# iptables -I INPUT 1 -j trafficMonitor_in
+# root@:~# iptables -I OUTPUT 1 -j trafficMonitor_out
 #
-# This assumes information is in third line like below. Edit line if this ins't the case
-# Chain INPUT (policy ACCEPT 0 packets, 0 bytes)
-# pkts      bytes target     prot opt in     out     source               destination
-# 1941988 1207873331 ACCEPT     all  --  *      *       0.0.0.0/0            0.0.0.0/0           state RELATED,ESTABLISHED
-# 627   183690 ACCEPT     all  --  eth0   *       0.0.0.0/0            0.0.0.0/0
-# 0        0 ACCEPT     all  --  eth0   *       0.0.0.0/0            0.0.0.0/0
-# 0        0 ACCEPT     all  --  *      *       0.0.0.0/0            0.0.0.0/0
-#
-# See your output by iptables -nx -vL <OUTPUT|INPUT>
-#
+# This assumes information is in third line like below. Edit line if this isn't the case.
+# Example:
+#	iptables -nx -vL INPUT
+# Chain DEFAULT_INPUT (1 references)
+    # pkts      bytes target     prot opt in     out     source               destination
+ # 1590475 85601301 trafficMonitor_in  all  --  *      *       0.0.0.0/0            0.0.0.0/0
 
+# Chain DEFAULT_OUTPUT (1 references)
+    # pkts      bytes target     prot opt in     out     source               destination
+  # 676941 10997095960 trafficMonitor_out  all  --  *      *       0.0.0.0/0            0.0.0.0/0
+
+#
+# See your output by iptables -nx -vL <OUTPUT|INPUT>. Data will be retrieved from bytes column
+#
+####
 # Another option is to monitor if certain ips are online, enter those in hosts. This setting overwrites
-# the minimum traffic treshold! This is meant as an addon.
+# the minimum traffic threshold!
 
 # USAGE
-# Following argument can be used: start, stop.
-# start for starting networkmonitor
-# stop for stopping a running networkmonitor
+# Following argument can be used: start, stop, test. Execute script without arguments for more info.
+
 #
 # Settings below
 #
 threshold=10 		# MB per interval
-monitor_time=5 		# mins per inverval
+monitor_time=4 		# mins per inverval
 times=3 		# number of times it should be below threshold in each interval
 lockfile="networkmonitor.lock"
 log="networkmonitor.log"
+debug="false"
+debuglog="debug.log"
 shutdown_command="poweroff"	#command to execute for shutdown
+enable="false" 		# (false|true)
 
-method="netcard" 	# iptables or netcard
+method="iptables" 	# iptables or netcard
 interface="eth0" 	# only for netcard.
-line="3"		# only for iptables
-add_iptables="no"	# add iptables -A INPUT -j ACCEPT and iptables -A OUTPUT -j ACCEPT to
-			# iptables upon script start
 
+# only for iptables
+iptableRulesNameIn="trafficMonitor_in"
+iptableRulesNameOut="trafficMonitor_out"
+add_iptables="true" # Will add rules on script start if they do not exists (false|true) add iptables -A INPUT -j ACCEPT and iptables -A OUTPUT -j ACCEPT to iptables upon script start. Requires sudo/root.
+
+# Monitor ip hosts
 hosts=""		# iphosts, seperate with :. Leave empty if not used. Format 192.168.1.1
-
-enable="0" 		# 1 the shutdown command is evaluated, 0 for testing purposes
 
 ################# CODE BELOW ################3
 
+if [[ $debug == "true" ]]; then
+	set -x
+	echo >> $debuglog
+	exec 2>> $debuglog
+	echo "STARTING PID=$$"
+fi
+
 control_c() {
-	# run if user hits control-c
-	rm "$lockfile"
-	exit 0
+# run if user hits control-c
+rm "$lockfile"
+exit 0
 }
 trap control_c SIGINT
 
+# Retrieve send bytes
 printrxbytes(){
-	if [ $method == "netcard" ]; then
-		ifconfig "$interface" | grep "RX bytes" | cut -d: -f2 | awk '{ print $1 }'
-	elif [ $method == "iptables" ]; then
-		iptables -nx -vL INPUT | cut -d' ' -f3 |  awk ' NR == 3'
-	fi
+if [[ $method == "netcard" ]]; then
+	ifconfig "$interface" | grep "RX bytes" | cut -d: -f2 | awk '{ print $1 }'
+elif [[ $method == "iptables" ]]; then
+	iptables -nx -vL | grep "$iptableRulesNameIn" |  awk '$1 ~ /^[0-9]+$/ { printf $2 }'
+fi
 }
 
+# Retrieve transferred bytes
 printtxbytes(){
-	if [ $method == "netcard" ]; then
-		ifconfig "$interface" | grep "TX bytes" | cut -d: -f3 | awk '{ print $1 }'
-	elif [ $method == "iptables" ]; then
-		iptables -nx -vL OUTPUT | cut -d' ' -f3 |  awk ' NR == 3'
-	fi
+if [[ $method == "netcard" ]]; then
+	ifconfig "$interface" | grep "TX bytes" | cut -d: -f3 | awk '{ print $1 }'
+elif [[ $method == "iptables" ]]; then
+	iptables -nx -vL | grep "$iptableRulesNameOut" |  awk '$1 ~ /^[0-9]+$/ { printf $2 }'
+fi
 }
+# Convert bytes to humanunits
 
 bytestohuman(){
-multiplier="0"
-number="$1"
-while [ "$number" -ge 1024 ] ; do
+local multiplier="0"
+local number="$1"
+while [[ "$number" -ge 1024 ]] ; do
 	multiplier=$(($multiplier+1))
 	number=$(($number/1024))
 done
 echo "$number"
 }
 
+# Convert bytes to humanunits
 bytestohumanunit(){
-multiplier="0"
-number="$1"
-while [ "$number" -ge 1024 ] ; do
+local multiplier="0"
+local number="$1"
+while [[ "$number" -ge 1024 ]]; do
 	multiplier=$(($multiplier+1))
 	number=$(($number/1024))
 done
@@ -115,31 +138,33 @@ esac
 echo "$unit"
 }
 
+# Print traffic result for manual monitoring infinite 
 printresults(){
-counter=30000
-while [ "$counter" -ge 0 ] ; do
+local counter=30000
+while [[ "$counter" -ge 0 ]]; do
 	counter=$(($counter - 1))
-	if [ "$rxbytes" ] ; then
+	if [[ "$rxbytes" ]]; then
 		oldrxbytes="$rxbytes"
 		oldtxbytes="$txbytes"
 	fi
 	rxbytes=$(printrxbytes)
 	txbytes=$(printtxbytes)
-	if [ "$oldrxbytes" -a "$rxbytes" -a "$oldtxbytes" -a "$txbytes" ] ; then
+	if [[ "$oldrxbytes" -gt 0 -a "$rxbytes" -gt 0 -a "$oldtxbytes" -gt 0 -a "$txbytes" -gt 0 ]]; then
 		echo "RXbytes = $(bytestohuman $(($rxbytes - $oldrxbytes))) $(bytestohumanunit $(($rxbytes - $oldrxbytes)))	TXbytes = $(bytestohuman $(($txbytes - $oldtxbytes))) $(bytestohumanunit $(($txbytes - $oldtxbytes)))"
 	else
-		echo "Monitoring $interface every $sleep seconds. (RXbyte total = $(bytestohuman $rxbytes) $(bytestohumanunit $(($rxbytes - $oldrxbytes))) TXbytes total = $(bytestohuman $txbytes)) $(bytestohumanunit $(($txbytes - $oldtxbytes)))"
+		echo "Monitoring $interface every 5 seconds. (RXbyte total = N/A TXbytes total = N/A"
 	fi
-	#sleep "$monitor_time"
-	sleep 20
+	sleep 5
 done
 }
 
+# Convert bytes to megabytes
 bytestomegabyts(){
 	number=$(($1/1024))
 	echo $number
 }
 
+# Shutdown logic
 shutdowntimer(){
 # Write startup message
 echo "Networking monitor $version"
@@ -159,34 +184,34 @@ while :; do
 		echo "Online hosts found: $ipfound"
 		low_times=0
 	else
-		if [ "$rxbytes" ]; then
+		if [[ "$rxbytes" ]]; then
 			oldrxbytes="$rxbytes"
 			oldtxbytes="$txbytes"
 		fi
 		rxbytes=$(printrxbytes)
 		txbytes=$(printtxbytes)
-		if [ "$oldrxbytes" -a "$rxbytes" -a "$oldtxbytes" -a "$txbytes" ] ; then
+		if [[ "$oldrxbytes" -gt 0 -a "$rxbytes" -gt 0 -a "$oldtxbytes" -gt 0 -a "$txbytes" -gt 0 ]]; then
 			rxunit=$(bytestohumanunit $(($rxbytes - $oldrxbytes)))
 			rxbytes_diff=$(bytestohuman $(($rxbytes - $oldrxbytes)))
 			txunit=$(bytestohumanunit $(($txbytes - $oldtxbytes)))
 			txbytes_diff=$(bytestohuman $(($txbytes - $oldtxbytes)))
 
 			# wrap around 2^32 count limit on 32bit systems. Reseting counter if lower!
-			if [ $rxbytes -lt $oldrxbytes ] || [ $txbytes -lt $oldtxbytes ]; then
+			if [[ $rxbytes -lt $oldrxbytes ]] || [[ $txbytes -lt $oldtxbytes ]]; then
 				low_times=0
-				if [ $rxbytes_diff -gt 0 ] && [ $txbytes_diff -gt 0 ]; then
+				if [[ $rxbytes_diff -gt 0 ]] && [[ $txbytes_diff -gt 0 ]]; then
 					echo "too high traffic.. RXbytes = adaptor reset N/A TXbytes = adaptor reset N/A"
-				elif [ $rxbytes_diff -gt 0 ] && [ $txbytes_diff -lt 0 ]; then
+				elif [[ $rxbytes_diff -gt 0 ]] && [[ $txbytes_diff -lt 0 ]]; then
 					 echo "too high traffic.. RXbytes = $rxbytes_diff $rxunit TXbytes = adaptor reset N/A"
-				elif [ $rxbytes_diff -lt 0 ] && [ $txbytes_diff -gt 0 ]; then
+				elif [[ $rxbytes_diff -lt 0 ]] && [[ $txbytes_diff -gt 0 ]]; then
 					 echo "too high traffic.. RXbytes = adaptor reset N/A TXbytes = $txbytes_diff $txunit"
 				fi
 				sleep $(( $monitor_time * 60 ))
 				continue
 			fi
 			# low activty counter
-			if [ $rxunit == "Mb" -o $rxunit == "Kb" -o $rxunit == "b" ] && [ $txunit == "Mb" -o $txunit == "Kb" -o $txunit == "b" ]; then
-				if [ $rxbytes_diff -gt "$threshold" -a $rxunit == "Mb" ] || [ $txbytes_diff -gt "$threshold" -a $txunit == "Mb" ]; then
+			if [[ $rxunit == "Mb" -o $rxunit == "Kb" -o $rxunit == "b" ]] && [[ $txunit == "Mb" -o $txunit == "Kb" -o $txunit == "b" ]]; then
+				if [[ $rxbytes_diff -gt "$threshold" -a $rxunit == "Mb" ]] || [[ $txbytes_diff -gt "$threshold" -a $txunit == "Mb" ]]; then
 					# Too much traffic
 					low_times=0
 					echo "too high traffic.. RXbytes = $rxbytes_diff $rxunit TXbytes = $txbytes_diff $txunit"
@@ -195,11 +220,11 @@ while :; do
 				fi
 				let low_times++
 				echo "too low traffic, $low_times times.. RXbytes = $rxbytes_diff $rxunit TXbytes = $txbytes_diff $txunit"
-				if [ $low_times -eq $times  ]; then
+				if [[ $low_times -eq $times  ]]; then
 					echo "time for shutdown"
 					rm "$lockfile"
 					echo "$(date): No network activity, shutting down.!" >> "$log"
-					if [ $enable -eq 1 ]; then
+					if [[ $enable == "true" ]]; then
 						eval "$shutdown_command"
 					fi
 					exit 0
@@ -216,6 +241,7 @@ while :; do
 done
 }
 
+# Manage lockfile
 lockfile(){
 if [[ -f "$lockfile" ]]; then
 	mypid_script=$(sed -n 1p "$lockfile")
@@ -231,11 +257,12 @@ if [[ -f "$lockfile" ]]; then
 else
 	echo $$ > "$lockfile"
 fi
-if [[ $add_iptables == "yes" ]] && [[ $method == "iptables" ]]; then
+if [[ $add_iptables == "true" ]] && [[ $method == "iptables" ]]; then
 	iptables_add
 fi
 }
 
+# Monitor ip's
 checkip(){
 ipfound=""
 iponline="no" # assume none is online
@@ -255,35 +282,47 @@ done
 IFS=$Old_ifs
 }
 
-# New to add the iptables
+# Populate iptables, so that we can get traffic information
 iptables_add(){
-	# check if rules exists
-	local var=$(iptables --list-rules | grep "\-A INPUT -j ACCEPT")
-	if [[ -z "$var" ]]; then
-		iptables -A INPUT -j ACCEPT
+	if [[ "$(id -u)" != "0" ]]; then
+		echo "To add entries to iptables, this script must be run as root"
+		exit 1
 	fi
-	unset var
-	local var=$(iptables --list-rules | grep "\-A OUTPUT -j ACCEPT")
+	# verify that rules exits, else create them
+	local var=$(iptables --list-rules | grep "\-A.*$iptableRulesNameIn")
 	if [[ -z "$var" ]]; then
-		iptables -A OUTPUT -j ACCEPT
+		iptables -N "$iptableRulesNameIn"
+		iptables -I INPUT 1 -j "$iptableRulesNameIn"
 	fi
-	unset var
+	local var=$(iptables --list-rules | grep "\-A.*$iptableRulesNameOut")
+	if [[ -z "$var" ]]; then
+		iptables -N "$iptableRulesNameOut"
+		iptables -I OUTPUT 1 -j "$iptableRulesNameOut"
+	fi
 }
 
 case $1 in
 	start)
 	lockfile
 	shutdowntimer
-	#printresults
+	;;
+	test)
+		printresults
+		exit 0
 	;;
 	stop)
-	if [ -f "$lockfile" ]; then
+	if [[ -f "$lockfile" ]]; then
 		mypid_script=$(sed -n 1p "$lockfile")
-	        kill -9 $mypid_script
+		kill -9 $mypid_script
 		rm "$lockfile"
 	else
 		echo "Not running"
 	fi
 	exit 0
 	;;
+	*)
+	echo "Usage:"
+	echo " start to execute"
+	echo " stop to terminate running process"
+	echo " test to monitor network activity for debug purposes"
 esac
