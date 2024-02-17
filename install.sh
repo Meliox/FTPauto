4c4aae1fdb555e1d21a62c0cf2809b4d88517e97
Meliox <silent.ftp@gmail.com>
2024-02-17 16:38:02 +0100

    install update

--- install.sh
+++ install.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
-###
+
+# Get the directory of the script
 scriptdir=$(dirname "$(readlink -f "$0")")
-#
-### code below
-#
+
+# Function to get the current version of FTPauto
 function getCurrentVersion {
-if [[ -f "$scriptdir/ftpauto.sh" ]]; then
-	# local version
-	i_version=$(sed -n '2p' "$scriptdir/ftpauto.sh")
-	i_version=${i_version#$"s_version=\""}
-	i_version=${i_version%\"}
-else
-	i_version=0
-fi
+	if [[ -f "$scriptdir/ftpauto.sh" ]]; then
+		# Get local version
+		i_version=$(sed -n '2p' "$scriptdir/ftpauto.sh")
+		i_version=${i_version#$"s_version=\""}
+		i_version=${i_version%\"}
+	else
+		i_version=0
+	fi
 }
-control_c() {
-	# run if user hits control-c
+
+# Function to handle Ctrl+C interruption
+function control_c() {
 	echo -ne '\n'
 	echo -e " \e[00;31mUser hit CTRL+C, exiting...\e[00m"
 	echo -e " Run install.sh uninstall to remove created files before trying again!\n"
@@ -23,10 +24,9 @@ control_c() {
 }
 trap control_c SIGINT
 
-#programs
+# Function to compare versions
 function version_compare {
 	if [[ "$1" == "$2" ]]; then
-		# Uptodate
 		new_version="false"
 	else
 		local IFS=.
@@ -42,14 +42,13 @@ function version_compare {
 	fi
 }
 
+# Function to install/update lftp
 function lftp_update {
 	argument="$1"
 	if [[ -n $(builtin type -p lftp) ]]; then
-		# lftp is installed, compare installed version to newest version
 		local lftpversion=$(curl --silent http://lftp.tech/ftp/ | grep -Eo '>lftp.(.*).+tar.gz<' | tail -1)
 		lftpversion=${lftpversion#\>lftp\-}
 		lftpversion=${lftpversion%.tar.gz<}
-		# get current lftp version
 		local c_lftpversion=$(lftp --version | grep -Eo 'Version\ [0-9].[0-9].[0-9]' | cut -d' ' -f2)
 		version_compare "$lftpversion" "$c_lftpversion"
 		if [[ "$new_version" == "true" ]]; then
@@ -58,7 +57,6 @@ function lftp_update {
 			if [[ "$REPLY" == "y" ]]; then
 				echo -n "Removing old version ..."
 				sudo apt-get -y remove lftp &> /dev/null
-				# remove compiled version
 				sudo rm -rf "$scriptdir/dependencies/lftp*"
 				argument="install"
 			else
@@ -73,7 +71,6 @@ function lftp_update {
 	if [[ "$argument" == "install" ]]; then
 		sudo apt-get -y build-dep lftp &> /dev/null
 		sudo apt-get -y install gcc openssl build-essential automake readline-common libreadline-dev pkg-config ncurses-dev libssl-dev libncurses5-dev libreadline-dev zlib1g-dev &> /dev/null
-		# find latest lftp
 		cd "$scriptdir/dependencies" || exit
 		local lftpversion=$(curl --silent http://lftp.tech/ftp/ | grep -Eo '>lftp.(.*).+tar.gz' | tail -1)
 		lftpversion=${lftpversion#\>lftp\-}
@@ -91,10 +88,11 @@ function lftp_update {
 	fi
 }
 
+# Function to install/update lftp
 function install_lftp {
 	echo -n " Checking/updating lftp ..."
 	if [[ -z $(builtin type -p lftp) ]]; then
-		echo -e "lftp is not installed! lftp is required for FTPauto to work!"
+		echo -e "lftp is not installed! It is required for FTPauto to work!"
 		read -p " Do you want to install it(y/n)? "
 		if [[ "$REPLY" == "y" ]]; then
 			read -p " Do you want latest version(needs to be compiled - SLOW - Any installed version will be removed) or the package from repo(y/n)? "
@@ -117,17 +115,16 @@ function install_lftp {
 			exit 0
 		fi
 	else
-		# lftp is installed, compare installed version to newest version
 		lftp_update
 	fi
 }
 
+# Function to install/update rar2fs
 function rar2fs_update {
 	argument="$1"
 	if [[ -n $(builtin type -p rar2fs) ]]; then
-		# rar2fs is installed, compare installed version to newest version
-		c_rar2fsversion=$(rar2fs --version 2> /dev/null | grep -Eo 'rar2fs\sv[0-9][0-9]?\.[0-9][0-9]?\.[0-9][0-9]?' | cut -d' ' -f2 | cut -d'v' -f2)
-		rar2fsversion=$(curl -s https://api.github.com/repos/hasse69/rar2fs/releases | grep browser_download_url | head -n 1 | cut -d '"' -f 4 | cut -d '/' -f8 | cut -d'v' -f2)
+		local c_rar2fsversion=$(rar2fs --version 2> /dev/null | grep -Eo 'rar2fs\sv[0-9][0-9]?\.[0-9][0-9]?\.[0-9][0-9]?' | cut -d' ' -f2 | cut -d'v' -f2)
+		local rar2fsversion=$(curl -s https://api.github.com/repos/hasse69/rar2fs/releases | grep browser_download_url | head -n 1 | cut -d '"' -f 4 | cut -d '/' -f8 | cut -d'v' -f2)
 		version_compare "$rar2fsversion" "$c_rar2fsversion"
 		if [[ "$new_version" == "true" ]]; then
 			echo -e " [\e[00;33m$rar2fsversion available, current version $c_rar2fsversion\e[00m]"
@@ -136,7 +133,6 @@ function rar2fs_update {
 				if [[ -n $(builtin type -p rar2fs) ]]; then
 					echo -n "Removing old version ..."
 					sudo apt-get -y remove rar2fs &> /dev/null
-					# remove compiled version
 					sudo rm -rf "$scriptdir/dependencies/rar2fs*"
 					argument="install"
 				fi
@@ -148,19 +144,15 @@ function rar2fs_update {
 		fi
 	fi
 	if [[ "$argument" == "install" ]]; then
-		# install
 		cd "$scriptdir/dependencies/"
-		# get latest stable release of rar2fs
-		var=$(curl -s https://api.github.com/repos/hasse69/rar2fs/releases | grep browser_download_url | head -n 1 | cut -d '"' -f 4)
+		local var=$(curl -s https://api.github.com/repos/hasse69/rar2fs/releases | grep browser_download_url | head -n 1 | cut -d '"' -f 4)
 		wget -q "$var"
-		name=$(basename "$var")
+		local name=$(basename "$var")
 		tar zxf "$name" && rm "$name"
 		name=${name::-7}
 		cd "$name"
-		#get unrar dependency for rar2fs
 		wget -q http://www.rarlab.com/rar/unrarsrc-5.6.3.tar.gz && tar -zxf unrarsrc-5.6.3.tar.gz && rm unrarsrc-5.6.3.tar.gz
 		cd unrar && make lib &> /dev/null && sudo make install-lib &> /dev/null && cd ..
-		#compile rar2fs
 		autoreconf -f -i &> /dev/null && ./configure --silent && make --silent && sudo checkinstall -y &> /dev/null
 		echo -n " Checking for rar2fs ..."
 		if [[ -n $(builtin type -p rar2fs) ]]; then
@@ -172,6 +164,7 @@ function rar2fs_update {
 	fi
 }
 
+# Function to install/update rar2fs
 function install_rar2fs {
 	echo -n " Checking/updating rar2fs ..."
 	if [[ -z $(builtin type -p rar2fs) ]]; then
@@ -188,20 +181,18 @@ function install_rar2fs {
 			echo -e "Checking rar2fs ... [\e[00;33mSKIPPED\e[00m] NOTE: \"video\" will not work as send option"
 		fi
 	elif [[ -n $(builtin type -p rar2fs) ]]; then
-		# rar2fs is installed, compare installed version to newest version
 		rar2fs_update
 	fi
 }
 
+# Function to install dependencies
 function installDependencies {
 	echo "Installing dependency tools ..."
-	# create directories
-	echo -n " Creating directories ..."
 	mkdir -p "$scriptdir/run" "$scriptdir/users"
+	echo -n " Creating directories ..."
 	echo -e "\e[00;32m [OK]\e[00m"
-	echo -n " Checking dependencies files ..."
+
 	local main_files=("ftpauto.sh" "dependencies/ftp_online_test.sh" "dependencies/ftp_list.sh" "dependencies/ftp_size_management.sh" "dependencies/help.sh" "plugins/largefile.sh" "dependencies/setup.sh" "dependencies/sorting.sh")
-	# Confirm all files are present
 	for i in "${main_files[@]}"; do
 		if [[ ! -f "$scriptdir/$i" ]]; then
 			echo -e "\n\e[00;31m$i not found.\nScript will not work without... Try reinstalling it. Exiting...\e[00m\n"; exit 1;
@@ -209,12 +200,10 @@ function installDependencies {
 	done
 	echo -e "\e[00;32m [OK]\e[00m"
 
-	# lftp
 	install_lftp
 
 	echo "Installing optional tools ..."
-	# split files
-	programs=( "rar" "cksfv" )
+	local programs=( "rar" "cksfv" )
 	for i in "${programs[@]}"; do
 		echo -n " Checking $i ..."
 		if [[ -z $(builtin type -p "$i") ]]; then
@@ -237,11 +226,10 @@ function installDependencies {
 			echo -e "\e[00;32m [OK]\e[00m"
 		fi
 	done
-	# for mounting to work
+
 	install_rar2fs
 
 	echo "Finalizing ..."
-	# Install default user
 	read -p " Do you want to install a user? (You can add user later on)(y/n)? "
 	if [[ "$REPLY" == "y" ]]; then
 		read -p " What username do you want to use (leave empty for "default")? "
@@ -271,15 +259,15 @@ function installDependencies {
 	exit 0
 }
 
+# Function to handle installation
 function install {
 	read -p " Do you wish to install(y/n)? "
 	if [[ "$REPLY" == "n" ]]; then
 		echo -e "... Exiting\n"; exit 0
 	fi
 
-	# Install mandatory things that is needed for rest to work
 	echo -e "\nInstalling minimum required tools ..."
-	programs=( "bc" "curl" "openssl" "date" "tail" "awk" "mkdir" "sed" "tput" "nano" "cut" "checkinstall")
+	local programs=( "bc" "curl" "openssl" "date" "tail" "awk" "mkdir" "sed" "tput" "nano" "cut" "checkinstall")
 	for i in "${programs[@]}"; do
 		echo -n " Checking for $i ..."
 		if [[ -z $(builtin type -p "$i") ]]; then
@@ -299,12 +287,12 @@ function install {
 			echo -e "\e[00;32m [OK]\e[00m"
 		fi
 	done
-	# ok we know have the required tools to update script
+
 	updateScript installNew
-	# continue  part2 of the installation
 	installDependencies
 }
 
+# Function to uninstall FTPauto
 function uninstall {
 	local programs=("lftp" "rar" "cksfv" "rar2fs" "pkg-config" "automake" "libfuse-dev" "checkinstall" "libssl-dev" "libncurses5-dev" "libreadline-dev" "zlib1g-dev" "autoconf")
 	echo "The following will be removed: ${programs[@]}"
@@ -344,6 +332,7 @@ function uninstall {
 	exit 0
 }
 
+# Function to handle downloading the script
 function downloadScript {
 	echo -n " Downloading FTPauto..."
 	wget -q "https://github.com/Meliox/FTPauto/archive/FTPauto-v$release_version.tar.gz"
@@ -358,56 +347,55 @@ function downloadScript {
 	exit 0;
 }
 
+# Function to handle updating the script
 function updateScript {
 	argument="$1"
 	getCurrentVersion
-	# get most recent stable version from git
 	echo -n " Checking/updating FTPauto ..."
 	local release=$(curl --silent https://github.com/Meliox/FTPauto/releases | grep -Eo 'FTPauto-v(.*)+tar.gz' | sort -n | tail -1)
 	release_version=${release#$"FTPauto-v"}
 	release_version=${release_version%.tar.gz}
 	version_compare "$release_version" "$i_version"
-	# compare to local version
 	if [[ "$i_version" == "0" ]] && [[ $argument != installNew ]]; then
 		echo -e "\e[00;31m [ERROR]\e[00m\nNo installation found. Execute script with install as argument instead. Exiting.\n"
 		exit 0
 	elif [[ "$new_version" == "true" ]] && [[ $argument == installNew ]]; then
 		echo -e "\e[00;32m [Found - v$release_version]\e[00m"
-		downloadScript
-	elif [[ "$new_version" == "true" ]]; then
-		echo -e "\e[00;33m [$release_version available]\e[00m"
-		read -p " Do you want to update your version(y/n)? "
+		read -p " Do you wish to update(y/n)? "
 		if [[ "$REPLY" == "y" ]]; then
 			downloadScript
 		else
-			echo -e "\e[00;33m [Local version kept]\e[00m"
+			echo -e " FTPauto update ... [\e[00;33mSKIPPED\e[00m]"
+		fi
+	elif [[ "$new_version" == "true" ]] && [[ $argument != installNew ]]; then
+		echo -e "\e[00;33m [Found - v$release_version]\e[00m"
+		read -p " Do you wish to update(y/n)? "
+		if [[ "$REPLY" == "y" ]]; then
+			downloadScript
+		else
+			echo -e " FTPauto update ... [\e[00;33mSKIPPED\e[00m]"
 		fi
 	else
-		echo -e "\e[00;32m [Latest - v$i_version]\e[00m"
-		sed "6c lastUpdate=$(date +'%s')" -i "$scriptdir/ftpauto.sh" # set update time
-		sed "7c message=\"\"" -i "$scriptdir/ftpauto.sh" # reset update message
+		if [[ "$argument" == "installNew" ]]; then
+			echo -e " \e[00;32m[Latest - v$release_version]\e[00m"
+		else
+			echo -e " \e[00;32m [Latest - v$i_version]\e[00m"
+		fi
 	fi
 }
 
-function update {
-	updateScript
-	install_rar2fs
-	install_lftp
-	echo -e "\n\e[00;32m[Update done]\e[00m\nEnjoy! Start using FTPauto by using ftpauto.sh --help\n"
-	exit 0
-}
-
-function startupmessage {
-	echo -e "\nAutoinstaller for FTPauto\n"
-}
-
-getCurrentVersion
 case "$1" in
-	uninstall) startupmessage; uninstall;;
-	install) startupmessage; install;;
-	update)	startupmessage; update;;
+	installNew)
+		updateScript installNew
+		;;
+	install)
+		install
+		;;
+	uninstall)
+		uninstall
+		;;
 	*)
-		startupmessage; echo -e "\nUsage: $0 (install | uninstall | update)\nExecute uninstall first to clean up everything if you encounter any problems.\n";
+		echo "Usage: $0 {install|uninstall}" >&2
 		exit 1
 		;;
 esac
